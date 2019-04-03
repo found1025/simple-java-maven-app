9ccc9eca59827fd72ca07c5642dc0bd2b1fd2341
found1025 <38653597+found1025@users.noreply.github.com>
2019-04-03 18:09:20 +0800

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -6,7 +6,7 @@ package com.mycompany.app;
 public class App
 {
 
-    private final String message = "Hello, the loving and fucking World!";
+    private final String message = "Hello, the lovly World!";
 
     public App() {}
 
